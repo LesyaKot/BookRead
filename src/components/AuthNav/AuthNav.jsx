4c3a88c29c307d102e0341ed462d8636de2f3f07
LesyaKot <lesyakot83@gmail.com>
2025-02-05 21:22:37 +0500

    add Header css, change AuthNav

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,56 +1,27 @@
 import { NavLink } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { logOut } from "../../redux/auth/operations";
-import { selectIsLoggedIn, selectUser } from "../../redux/auth/selectors";
 import css from "./AuthNav.module.css";
 
-const getFirstName = (fullName) => (fullName ? fullName.split(" ")[0] : "User");
-
 export default function AuthNav() {
-  const loggedIn = useSelector(selectIsLoggedIn);
-  const dispatch = useDispatch();
-  const user = useSelector(selectUser);
-
-  const handleLogout = () => {
-    dispatch(logOut());
-  };
-
   return (
     <div className={css.wrap}>
-      {!loggedIn ? (
-        <>
-          <NavLink
-            to="/register"
-            className={({ isActive }) =>
-              isActive ? `${css.link} ${css.active}` : css.link
-            }
-          >
-            Register
-          </NavLink>
-          <NavLink
-            to="/login"
-            className={({ isActive }) =>
-              isActive ? `${css.link} ${css.active}` : css.link
-            }
-          >
-            Log in
-          </NavLink>
-        </>
-      ) : (
-        <div>
-          {user?.name ? (
-            <>
-              <div>{user.name.charAt(0).toUpperCase()}</div>
-              <h3>{getFirstName(user.name)}</h3>
-            </>
-          ) : (
-            <h3>No name</h3>
-          )}
-          <button className={css.logoutBtn} onClick={handleLogout}>
-            Log out
-          </button>
-        </div>
-      )}
+      <>
+        <NavLink
+          to="/register"
+          className={({ isActive }) =>
+            isActive ? `${css.link} ${css.active}` : css.link
+          }
+        >
+          Register
+        </NavLink>
+        <NavLink
+          to="/login"
+          className={({ isActive }) =>
+            isActive ? `${css.link} ${css.active}` : css.link
+          }
+        >
+          Log in
+        </NavLink>
+      </>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,24 +2,59 @@ import { MdMenuBook } from "react-icons/md";
 import { IoHomeOutline } from "react-icons/io5";
 import { NavLink } from "react-router-dom";
 import Logo from "../Logo/Logo";
-import AuthNav from "../AuthNav/AuthNav";
+
 import css from "./Header.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { logOut } from "../../redux/auth/operations";
+import { selectUser } from "../../redux/auth/selectors";
+
+const getFirstName = (fullName) => (fullName ? fullName.split(" ")[0] : "User");
 
 export default function Header() {
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+
+  const handleLogout = () => {
+    dispatch(logOut());
+  };
   return (
     <>
-      <Logo />
+      <div className={css.wrap}>
+        <Logo />
 
-      <nav className={css.nav}>
-        <NavLink className={css.link} to="/">
-          <IoHomeOutline />
-        </NavLink>
-        <NavLink className={css.link} to="/library">
-          <MdMenuBook />
-        </NavLink>
-      </nav>
+        <nav className={css.nav}>
+          <NavLink className={css.link} to="/library">
+            <div className={css.iconBookWrap}>
+              <MdMenuBook className={css.iconHome} width={22} height={17} />
+            </div>
+          </NavLink>
 
-      <AuthNav />
+          <NavLink className={css.link} to="/">
+            <div className={css.iconHomeWrap}>
+              <IoHomeOutline className={css.iconBook} width={22} height={17} />
+            </div>
+          </NavLink>
+        </nav>
+
+        <div className={css.nameWrap}>
+          {user?.name ? (
+            <>
+              <div className={css.firstLetterWrap}>
+                <p className={css.firstLetterWrap}>
+                  {user.name.charAt(0).toUpperCase()}
+                </p>
+              </div>
+
+              <p className={css.userName}>{getFirstName(user.name)}</p>
+            </>
+          ) : (
+            <h3>No name</h3>
+          )}
+          <button className={css.logoutBtn} onClick={handleLogout}>
+            Log out
+          </button>
+        </div>
+      </div>
     </>
   );
 }
